b2313e15812d201550ff0d58cc30e00821133965
phothinh <phothinh01@gmail.com>
2024-01-10 09:52:23 +0100

    Initial Commit

---
+++ Looter/LooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooterApp.swift
+//  Looter
+//
+//  Created by PHOTHINANTHA Henri on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
